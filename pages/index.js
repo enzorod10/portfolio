4850ee6e4f79dee2ce419840bdf1395d1a22d7b4
enzo <me@enzorod.com>
2024-11-10 23:32:41 -0500

    began working on version 2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react';
-import Intro from '../src/components/Intro/Intro';
+import { useState } from 'react';
 import About from '../src/components/About/About';
 import PortfolioHeader from '../src/components/PortfolioHeader/PortfolioHeader';
 import Contact from '../src/components/Contact/Contact';
@@ -8,52 +7,9 @@ import Project1 from '../src/components/Project1/Project1';
 import Project2 from '../src/components/Project2/Project2';
 import Project3 from '../src/components/Project3/Project3';
 import Head from 'next/head';
+import Scene from '../src/components/Scene';
 
-function App({ windowSize }) {
-    // Depending on scroll location, header will hide or show
-    useEffect(() => {
-      const location = document.querySelector('.aboutSection').getBoundingClientRect().top
-      if (location < 0){
-        document.querySelector('.header').classList.remove('hide')
-      }
-      window.addEventListener('scroll', handleScroll, { passive: true })
-      return () => window.removeEventListener('scroll', handleScroll)
-    }, [])
-  
-    const handleScroll = () => {
-      if (document.querySelector('.aboutSection')){
-        const location = document.querySelector('.aboutSection').getBoundingClientRect().top
-        if (location){
-          if (location > 285 && !document.querySelector('.header').classList.contains('hide')){
-            document.querySelector('.header').classList.add('hide')
-          } 
-          else if (location < 220 && document.querySelector('.header').classList.contains('hide')){
-            document.querySelector('.header').classList.remove('hide')
-          }
-        }
-      } 
-    }
-
-    const scrollIntoDiv = destination => {
-      if (destination === 'home'){
-        window.scrollTo({top: 0, left: 0, behavior: 'smooth'})
-      }
-      if (destination === 'about'){
-        const location = document.querySelector('.header').classList.contains('hide') ? document.querySelector('.aboutSection').getBoundingClientRect().top + window.scrollY - 0.6 : document.querySelector('.aboutSection').getBoundingClientRect().top + window.scrollY - 65;
-        window.scrollTo({top: location, behavior: 'smooth'})
-      }
-      if (destination === 'projects'){
-        const location = document.querySelector('.header').classList.contains('hide') ? document.querySelector('.mainSection').getBoundingClientRect().top + window.scrollY - 30 : document.querySelector('.mainSection').getBoundingClientRect().top + window.scrollY - 65;
-        window.scrollTo({top: location, behavior: 'smooth'})
-      }
-      if (destination === 'contact'){
-        const location = document.querySelector('.header').classList.contains('hide') ? document.querySelector('.contactSection').getBoundingClientRect().top + window.scrollY - 30 : document.querySelector('.contactSection').getBoundingClientRect().top + window.scrollY - 65;
-        window.scrollTo({top: location, behavior: 'smooth'})
-      }
-    }
-
-  // For smaller screen widths, expand/collapse system on projects
-
+function App() {
   const [expandedProject, setExpandedProject] = useState({
     project0: false,
     project1: false,
@@ -61,43 +17,30 @@ function App({ windowSize }) {
     project3: false,
   })
 
-  const handleExpandCollapse = (projectName) => {
-    let tempExpandedProject = {...expandedProject}
-
-    for (let x in expandedProject){
-      if (x === projectName){
-        !tempExpandedProject[x] ? tempExpandedProject[x] = true : tempExpandedProject[x] = false
-      } else {
-        tempExpandedProject[x] = false
-      }
-    }
-    
-    setExpandedProject(tempExpandedProject)
-  }
-
   return (
     <>
     <Head>
       <title>Portfolio</title>
     </Head>
-    <div className="App">
-      <Intro scrollIntoDiv={scrollIntoDiv}/>
-      <PortfolioHeader windowSize={windowSize} scrollIntoDiv={scrollIntoDiv} />
-      <div className='aboutSection'>
-        <About/>
-      </div>
-      <div className='mainSection'>
-        <div style={{display: windowSize.width && windowSize.width > 640 ? 'none' : 'block'}} className={'projectSectionHeader'}>Projects</div>
+    <div className="App p-4">
+      <PortfolioHeader />
+      <About/>
+      
+      {/* <div className='flex flex-col gap-4 p-4 bg-[#1a1a1a]'>
         <Project3 windowSize={windowSize} handleExpandCollapse={handleExpandCollapse} expandedProject={expandedProject} />
         <Project0 windowSize={windowSize} handleExpandCollapse={handleExpandCollapse} expandedProject={expandedProject} />
         <Project1 windowSize={windowSize} handleExpandCollapse={handleExpandCollapse} expandedProject={expandedProject} />
         <Project2 windowSize={windowSize} handleExpandCollapse={handleExpandCollapse} expandedProject={expandedProject} />
         <div className='projectOverlay'>
         </div>
+      </div> */}
+      <div className='border absolute h-full'>
+        proj
+        <Scene />
       </div>
-      <div className='contactSection'>
+      {/* <div className='contactSection'>
         <Contact />
-      </div>
+      </div> */}
     </div>
     </>
   );
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,79 +1,53 @@
 import styles from './About.module.css';
-import sampleImage from '../../../public/assets/sampleImage.png';
 import nodejsIcon from '../../../public/assets/stackImages/nodejsIcon.svg';
 import mongodbIcon from '../../../public/assets/stackImages/mongodbIcon.svg';
 import gitIcon from '../../../public/assets/stackImages/gitIcon.svg';
-import jestIcon from '../../../public/assets/stackImages/jestIcon.svg';
 import tailwindIcon from '../../../public/assets/stackImages/tailwindIcon.svg';
 import nextjsIcon from '../../../public/assets/stackImages/nextjsIcon.svg';
 import reduxIcon from '../../../public/assets/stackImages/reduxIcon.svg';
-import blenderIcon from '../../../public/assets/stackImages/blenderIcon.svg';
 import typescriptIcon from '../../../public/assets/stackImages/typescriptIcon.svg';
 import psqlIcon from '../../../public/assets/stackImages/psqlIcon.svg';
+import Image from 'next/image';
+
+const techIcons = [
+    { name: 'Typescript', img: typescriptIcon.src },
+    { name: 'NextJS', img: nextjsIcon.src },
+    { name: 'PostgreSQL', img: psqlIcon.src },
+    { name: 'Redux', img: reduxIcon.src },
+    { name: 'Tailwind', img: tailwindIcon.src },
+    { name: 'NodeJS', img: nodejsIcon.src },
+    { name: 'MongoDB', img: mongodbIcon.src },
+    { name: 'Git', img: gitIcon.src }
+]
 
 function About(){
     return(
-        <div className={styles.fullPage}>
-            <div className={styles.top}>
-                <div className={styles.leftSide}>
-                    <img className={styles.image} src={sampleImage.src} alt='sample' />
-                </div>
-                <div className={styles.rightSide}>
-                    <div className={styles.infoHeader}>
+        <div className='flex flex-col gap-4 max-w-4xl mx-auto'>
+            <div className='relative mx-auto flex gap-4 items-center'>
+                <img src='/assets/sampleImage.png' className='w-36 sm:w-full sm:max-w-[240px]' alt=''/>
+                <div className='color: #007989'>
+                    <div className='text-[6vmin] text-black'>
                         Hello there
                     </div>
-                    <div className={styles.infoBody}>
+                    <div className='text-[3vmin] text-[#007989]'>
                         I'm Enzo Rodriguez, a New Jersey-based developer with experience
                         in the frontend and backend. I enjoy bringing ideas to life in the browser 
                         through code.
                     </div>
                 </div>
             </div>
-            <div className={styles.middle}>
-                <div className={styles.iconsContainer}>
-                    <div className={styles.iconsContainerTitle}>
+            <div className=''>
+                <div className='relative flex flex-wrap justify-center pt-4 pb-2 px-4 border-2 rounded-2xl gap-x-4 gap-y-2'>
+                    <div className='absolute -top-[12px] z-1 bg-white px-3 text-md text-[#007989]'>
                         Languages & Tools
                     </div>
-                    <div className={styles.techIconContainer}>
-                        <img className={styles.techIcon} src={typescriptIcon.src} alt='typescript'/>
-                        <div className={styles.techText}>TypeScript</div>
-                    </div>
-                    <div className={styles.techIconContainer}>
-                        <img className={styles.techIcon} src={nextjsIcon.src} alt='nextjs'/>
-                        <div className={styles.techText}>NextJs</div>
-                    </div>
-                    <div className={styles.techIconContainer}>
-                        <img className={styles.techIcon} src={psqlIcon.src} alt='postgreSQL'/>
-                        <div className={styles.techText}>PostgreSQL</div>
-                    </div>
-                    <div className={styles.techIconContainer}>
-                        <img className={styles.techIcon} src={reduxIcon.src} alt='redux'/>
-                        <div className={styles.techText}>Redux</div>
-                    </div>
-                    <div className={styles.techIconContainer}>
-                        <img className={styles.techIcon} src={tailwindIcon.src} alt='tailwind'/>
-                        <div className={styles.techText}>Tailwind</div>
-                    </div>
-                    <div className={styles.techIconContainer}>
-                        <img className={styles.techIcon} src={nodejsIcon.src} alt='nodejs'/>
-                        <div className={styles.techText}>NodeJS</div>
-                    </div>
-                    <div className={styles.techIconContainer}>
-                        <img className={styles.techIcon} src={blenderIcon.src} alt='nextjs'/>
-                        <div className={styles.techText}>Blender</div>
-                    </div>
-                    <div className={styles.techIconContainer}>
-                        <img className={styles.techIcon} src={mongodbIcon.src} alt='mongodb'/>
-                        <div className={styles.techText}>MongoDB</div>
-                    </div>
-                    <div className={styles.techIconContainer}>
-                        <img className={styles.techIcon} src={jestIcon.src} alt='jest'/>
-                        <div className={styles.techText}>Jest</div>
-                    </div>
-                    <div className={styles.techIconContainer}>
-                        <img className={styles.techIcon} src={gitIcon.src} alt='git'/>
-                        <div className={styles.techText}>Git</div>
-                    </div>
+                    {techIcons.map(tech => {
+                        return (<div key={tech.name} className='flex  flex-col justify-center items-center gap-1'>
+                            <img className='w-6 md:w-8 ' src={tech.img} alt={tech.name}/>
+                            <div className='text-xs text-[#007989]'>{tech.name}</div>
+                        </div>)
+                    })}
+                 
                 </div>
             </div>
         </div>
