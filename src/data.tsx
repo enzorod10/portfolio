637f7d313433190fd92a38cda29ceec3898fca80
enzo <me@enzorod.com>
2024-12-14 23:50:14 -0500

    began adding array for projects

---
+++ src/data.tsx
@@ -0,0 +1,9 @@
+const projects = [
+    {
+        name: 'Kick Realm',
+        info: "A platform for Kick streamers and viewers, where users can invest in streamers using virtual currency, track real-time analytics, and view upcoming stream schedules. Streamers can apply for affiliation, and users can apply for admin or mod roles, fostering a vibrant and interactive community.",
+        liveLink: '',
+        codeLink: '',
+        techUsed: ''
+    }
+]
