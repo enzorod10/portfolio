43827ec43142ce1e117bb422b1ecaff621659a3e
enzo <me@enzorod.com>
2025-01-27 23:25:19 -0500

    updated logo

--- src/components/PortfolioHeader.tsx
+++ src/components/PortfolioHeader.tsx
@@ -1,10 +1,11 @@
-import blackLogo from '../../public/assets/enzo-cs-logo-black.png'
+// import blackLogo from '../../public/assets/enzo-cs-logo-black.png'
+import logo from '../../public/assets/enzo-cs-logo.png'
 
 function PortfolioHeader(props){
   return(
       <div className='header select-none justify-between items-center flex flex-row z-50 h-16 px-2 sm:px-4 sticky top-0 '>
         <div className='logo' >
-          <img src={blackLogo.src} alt='Enzo Logo' />
+          <img className='h-full w-24 sm:w-32' src={logo.src} alt='Enzo Logo' />
         </div>
         <div className='text-black directory text-lg hidden sm:flex items-center'>
           <div onClick={() => props.scrollIntoDiv('about')}>
